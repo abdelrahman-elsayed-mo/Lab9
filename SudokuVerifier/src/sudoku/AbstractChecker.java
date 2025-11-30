8a752c2e72efec8cca7dfaf6f5268f0206a8a1a9
Youssef Fathy <yousseffathy771@gmail.com>
2025-12-01 01:05:14 +0200

    added AbstractChecker class

--- SudokuVerifier/src/sudoku/AbstractChecker.java
+++ SudokuVerifier/src/sudoku/AbstractChecker.java
@@ -16,5 +16,39 @@ import java.util.List;
 import java.util.Map;
 
 public abstract class AbstractChecker implements Checker {
-  
+  protected int[][] board;
+    protected Map<String, List<DuplicateInfo>> errors;
+    protected int index; // 0 means all, else specific 1-9
+
+    public AbstractChecker(int[][] board, Map<String, List<DuplicateInfo>> errors, int index) {
+        this.board = board;
+        this.errors = errors;
+        this.index = index;
+    }
+
+    @Override
+    public void run() {
+        check();
+    }
+
+    protected void findDuplicates(int[] arr, String key, int offset) {
+        Map<Integer, List<Integer>> freq = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            int num = arr[i];
+            freq.computeIfAbsent(num, k -> new ArrayList<>()).add(i + offset);
+        }
+
+        List<DuplicateInfo> infos = new ArrayList<>();
+        for (Map.Entry<Integer, List<Integer>> entry : freq.entrySet()) {
+            if (entry.getValue().size() > 1) {
+                DuplicateInfo info = new DuplicateInfo(entry.getKey());
+                info.positions.addAll(entry.getValue());
+                infos.add(info);
+            }
+        }
+
+        if (!infos.isEmpty()) {
+            errors.put(key, infos);
+        }
+    }
 }
